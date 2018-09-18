5d9f3b11b2d53734e88addaa9051f1a9d46c3629
aishborawar99 <42371457+aishborawar99@users.noreply.github.com>
2018-09-18 19:51:18 +0530

    Add files via upload

--- sparse.js
+++ sparse.js
@@ -1,5 +1,8 @@
-
-
+/**
+* @ method checkSparseOrNot
+* @param {number} 
+* @returns {number is sparse} {number is not sparse}
+*/
 function checkSparseOrNot(number){
 	if((number & (number>>1))>=1){
 		return 'number is sparse';
